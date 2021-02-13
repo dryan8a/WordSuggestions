82758e04d233af2d9937f3773dd9a9835d4efbd3
dryan8a@gmail.com <dryan8a@gmail.com>
2021-02-13 14:16:09 -0800

    Realized I actually didn't want exactly a hash map, as I didn't needed to hash my values and thought it would be best to avoid possible collisions. So I instead made a dictonary of the soundex string to a list of words who share that soundex. Using this I was able to implement the spelling suggestions box, which uses the soundex comparison. I also made the amount of suggestions larger, provided an option to display all of the suggestions from both boxes, and have a checkbox that tells a user if their word is valid according to the database of words. Next time, look into making the spelling suggestions more helpful by eliminating words that, despite sounding similar are spelled too differently to be what the user intends

--- WordSuggestions/Form1.Designer.cs
+++ WordSuggestions/Form1.Designer.cs
@@ -34,13 +34,15 @@ namespace WordSuggestions
             this.CompletionSuggestions = new System.Windows.Forms.ListBox();
             this.label2 = new System.Windows.Forms.Label();
             this.SpellingSuggestions = new System.Windows.Forms.ListBox();
+            this.ValidWordCheckBox = new System.Windows.Forms.CheckBox();
+            this.ScrollBarCheckBox = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // InputBox
             // 
             this.InputBox.Location = new System.Drawing.Point(12, 12);
             this.InputBox.Name = "InputBox";
-            this.InputBox.Size = new System.Drawing.Size(657, 426);
+            this.InputBox.Size = new System.Drawing.Size(467, 426);
             this.InputBox.TabIndex = 0;
             this.InputBox.Text = "";
             this.InputBox.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
@@ -48,7 +50,7 @@ namespace WordSuggestions
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(675, 31);
+            this.label1.Location = new System.Drawing.Point(649, 15);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(120, 13);
             this.label1.TabIndex = 2;
@@ -57,15 +59,15 @@ namespace WordSuggestions
             // CompletionSuggestions
             // 
             this.CompletionSuggestions.FormattingEnabled = true;
-            this.CompletionSuggestions.Location = new System.Drawing.Point(675, 47);
+            this.CompletionSuggestions.Location = new System.Drawing.Point(652, 31);
             this.CompletionSuggestions.Name = "CompletionSuggestions";
-            this.CompletionSuggestions.Size = new System.Drawing.Size(113, 95);
+            this.CompletionSuggestions.Size = new System.Drawing.Size(136, 329);
             this.CompletionSuggestions.TabIndex = 4;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(675, 237);
+            this.label2.Location = new System.Drawing.Point(485, 15);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(105, 13);
             this.label2.TabIndex = 5;
@@ -74,16 +76,40 @@ namespace WordSuggestions
             // SpellingSuggestions
             // 
             this.SpellingSuggestions.FormattingEnabled = true;
-            this.SpellingSuggestions.Location = new System.Drawing.Point(678, 254);
+            this.SpellingSuggestions.Location = new System.Drawing.Point(488, 31);
             this.SpellingSuggestions.Name = "SpellingSuggestions";
-            this.SpellingSuggestions.Size = new System.Drawing.Size(110, 95);
+            this.SpellingSuggestions.Size = new System.Drawing.Size(136, 329);
             this.SpellingSuggestions.TabIndex = 6;
+            // 
+            // ValidWordCheckBox
+            // 
+            this.ValidWordCheckBox.AutoCheck = false;
+            this.ValidWordCheckBox.AutoSize = true;
+            this.ValidWordCheckBox.Location = new System.Drawing.Point(575, 366);
+            this.ValidWordCheckBox.Name = "ValidWordCheckBox";
+            this.ValidWordCheckBox.Size = new System.Drawing.Size(114, 17);
+            this.ValidWordCheckBox.TabIndex = 8;
+            this.ValidWordCheckBox.Text = "Is your word valid?";
+            this.ValidWordCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // ScrollBarCheckBox
+            // 
+            this.ScrollBarCheckBox.AutoSize = true;
+            this.ScrollBarCheckBox.Location = new System.Drawing.Point(485, 421);
+            this.ScrollBarCheckBox.Name = "ScrollBarCheckBox";
+            this.ScrollBarCheckBox.Size = new System.Drawing.Size(171, 17);
+            this.ScrollBarCheckBox.TabIndex = 9;
+            this.ScrollBarCheckBox.Text = "Allow for Scrollbar Suggestions";
+            this.ScrollBarCheckBox.UseVisualStyleBackColor = true;
+            this.ScrollBarCheckBox.CheckedChanged += new System.EventHandler(this.ScrollBarCheckBox_CheckedChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ScrollBarCheckBox);
+            this.Controls.Add(this.ValidWordCheckBox);
             this.Controls.Add(this.SpellingSuggestions);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.CompletionSuggestions);
@@ -104,6 +130,8 @@ namespace WordSuggestions
         private System.Windows.Forms.ListBox CompletionSuggestions;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ListBox SpellingSuggestions;
+        private System.Windows.Forms.CheckBox ValidWordCheckBox;
+        private System.Windows.Forms.CheckBox ScrollBarCheckBox;
     }
 }
 
--- WordSuggestions/Form1.cs
+++ WordSuggestions/Form1.cs
@@ -15,9 +15,13 @@ namespace WordSuggestions
 {
     public partial class Form1 : Form
     {
-        int NumberOfSuggestions = 7;
+        int NumberOfSuggestionsCap = 25;
+        bool CapSuggestions = true;
+        int MaxSpellingDifferences = 10;
+
         string[] words;
         Trie wordsTrie = new Trie();
+        Dictionary<string, List<string>> soundexToWords = new Dictionary<string, List<string>>();
 
         public Form1()
         {
@@ -29,27 +33,80 @@ namespace WordSuggestions
             CompletionSuggestions.Items.Clear();
             SpellingSuggestions.Items.Clear();
             string[] words = File.ReadAllLines("words_alpha.txt");
-            foreach(var word in words)
+            foreach(var word in words) //fills the wordsTrie and soundexToWords
             {
                 wordsTrie.Add(word);
+
+                string soundex = Soundex.GetSoundex(word);
+                if(soundexToWords.ContainsKey(soundex))
+                {
+                    soundexToWords[soundex].Add(word);
+                }
+                else
+                {
+                    soundexToWords.Add(soundex, new List<string>() { word });
+                }
             }
         }
 
         private void richTextBox1_TextChanged(object sender, EventArgs e)
         {
+            UpdateSuggestions();
+        }
+
+        private void UpdateSuggestions()
+        {
             CompletionSuggestions.Items.Clear();
             SpellingSuggestions.Items.Clear();
+            ValidWordCheckBox.Checked = false;
 
             string currentWord = (InputBox.Text.Contains(' ') ? InputBox.Text.Substring(InputBox.Text.LastIndexOf(' ')) : InputBox.Text).Trim(' '); //gets the last or only word from the input
-            if (currentWord == "" || Regex.IsMatch(currentWord,"/[^A-Za-z]")) return; 
+            if (currentWord == "" || Regex.IsMatch(currentWord, "/[^A-Za-z]")) return;
             var completionSuggestions = wordsTrie.GetAllWordsMatchingPrefix(currentWord);
 
-            for(int i = 0;i<NumberOfSuggestions;i++)
+
+            if (completionSuggestions.Contains(currentWord.ToLower()))
             {
-                if (completionSuggestions.Count() <= i) break;
+                ValidWordCheckBox.Checked = true;
+            }
+
+            for (int i = 0; i < NumberOfSuggestionsCap; i++)
+            {
+                if (i >= completionSuggestions.Count()) break;
                 CompletionSuggestions.Items.Add(completionSuggestions.ElementAt(i));
+            }
+
+            string wordSoundex = Soundex.GetSoundex(currentWord);
+            if (soundexToWords.ContainsKey(wordSoundex))
+            {
+                AddItemsToSpellingSuggestions(soundexToWords[wordSoundex]);
+            }
+            foreach (var keySoundex in soundexToWords.Keys)
+            {
+                if (CapSuggestions && SpellingSuggestions.Items.Count == NumberOfSuggestionsCap) break;
+                if (Soundex.FindSoundexDifference(keySoundex, wordSoundex) == 3)
+                {
+                    AddItemsToSpellingSuggestions(soundexToWords[keySoundex]);
+                }
             }
         }
 
+        private void AddItemsToSpellingSuggestions(List<string> words)
+        {
+            if (CapSuggestions && words.Count > NumberOfSuggestionsCap - SpellingSuggestions.Items.Count)
+            {
+                SpellingSuggestions.Items.AddRange(words.GetRange(0, NumberOfSuggestionsCap - SpellingSuggestions.Items.Count).ToArray());
+            }
+            else
+            {
+                SpellingSuggestions.Items.AddRange(words.ToArray());
+            }
+        }
+
+        private void ScrollBarCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            CapSuggestions = !ScrollBarCheckBox.Checked;
+            UpdateSuggestions();
+        }
     }
 }
