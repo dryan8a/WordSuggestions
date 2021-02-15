96fcd8431f3c8761bbc3483251d7d114ba278933
dryan8a@gmail.com <dryan8a@gmail.com>
2021-02-14 20:39:07 -0800

    did some stuff

---
+++ WordSuggestions/WordDifferentiator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordSuggestions
+{
+    public static class WordDifferentiator
+    {
+        //public static int GetNumberOfDifferences(string left, string right)
+        //{
+        //    int differences = Math.Abs(left.Length - right.Length);
+        //    for(int i = 0;i < left.Length > right.Length )
+        //}
+    }
+}
