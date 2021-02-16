6068b2eee1e1d1b1220590aa05d0ee3d82bd1252
dryan8a@gmail.com <dryan8a@gmail.com>
2021-02-15 18:40:35 -0800

    Implemented Levenshtein distance in a static Levenshtein class and used it to condense the amount of displayed spelling suggestions. I find the ratio of the amount differences in the entered and suggested word to the maximum possible difference they can have. This gives a metric of how different two words are without over regulating smaller words or under regulating larger words. I then have a const double that holds the max acceptable ratio to display. I have found, as expected, that implementing this does dramatically slow performance, so next time I will look into ways of improving the speed. I have found an alternate implementation of the Levenshtein distance algo that uses less memory and theoretically has less loops, though it does keep its Big O complexity. I will try to implement that and maybe compare the algos' using a benchmarker

---
+++ WordSuggestions/Levenshtein.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordSuggestions
+{
+    public static class Levenshtein
+    {
+        /// <summary>
+        /// Gets Levenshtein distance of two strings using the full matrix implementation
+        /// </summary>
+        /// <param name="left">The first string to compare</param>
+        /// <param name="right">The second string to compare</param>
+        /// <returns></returns>
+        public static int GetFullMatrixDistance(string left, string right)
+        {
+            var matrix = new int[left.Length+1,right.Length+1];
+            for(int i = 0;i<=left.Length;i++)
+            {
+                matrix[i, 0] = i;
+            }
+            for(int i = 1;i<=right.Length;i++)
+            {
+                matrix[0, i] = i;
+            }
+
+            for(int n = 1;n<=right.Length;n++)
+            {
+                for(int m = 1;m<=left.Length;m++)
+                {
+                    int subCost = left[m-1] == right[n-1] ? 0 : 1;
+                    matrix[m, n] = Math.Min(matrix[m - 1, n] + 1, Math.Min(matrix[m, n - 1] + 1, matrix[m - 1, n - 1] + subCost));
+                }
+            }
+
+            return matrix[left.Length, right.Length];
+        }
+
+        /// <summary>
+        /// Gets the Maximum Possible Levenshtein distance between two words
+        /// </summary>
+        /// <param name="left">The first string to compare</param>
+        /// <param name="right">The second string to compare</param>
+        /// <returns></returns>
+        public static int GetMaxDistance(string left, string right)
+        {
+            return left.Length > right.Length ? left.Length : right.Length;
+        }
+
+        /// <summary>
+        /// Gets the Ratio of the Actual Levenshtein distance to the Max Levenshtein distance of two words
+        /// </summary>
+        /// <param name="left">The first string to compare</param>
+        /// <param name="right">The second string to compare</param>
+        /// <returns></returns>
+        public static double GetFullMatrixLevenshteinRatio(string left,string right)
+        {
+            return (double)GetFullMatrixDistance(left, right) / GetMaxDistance(left, right);
+        }
+    }
+}
--- WordSuggestions/Soundex.cs
+++ WordSuggestions/Soundex.cs
@@ -8,6 +8,11 @@ namespace WordSuggestions
 {
     public static class Soundex
     {
+        /// <summary>
+        /// Gets the soundex of a word
+        /// </summary>
+        /// <param name="word">the word to get the soundex of</param>
+        /// <returns></returns>
         public static string GetSoundex(string word)
         {
             if (string.IsNullOrWhiteSpace(word)) return "";
@@ -34,6 +39,11 @@ namespace WordSuggestions
             return equivalentSoundex.ToString();
         }
 
+        /// <summary>
+        /// Part of the Soundex Algo responsible for converting a character to its respective Soundex number
+        /// </summary>
+        /// <param name="character">The character to convert</param>
+        /// <returns></returns>
         private static char GetEquivalentNumber(char character)
         {
             if("bfpv".IndexOf(character) > -1)
@@ -63,6 +73,12 @@ namespace WordSuggestions
             return ' '; //returns ' ' for invalid or ignored characters
         }
 
+        /// <summary>
+        /// Finds the Soundex difference of two Soundex
+        /// </summary>
+        /// <param name="soundex1">The first Soundex to compare</param>
+        /// <param name="soundex2">The second Soundex to compare</param>
+        /// <returns></returns>
         public static int FindSoundexDifference(string soundex1, string soundex2)
         {
             if(soundex1 == soundex2)
