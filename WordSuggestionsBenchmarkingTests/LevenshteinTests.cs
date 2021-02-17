7d53d8ba9e4ed0ff3910cfa3d30d675bb56e3e94
dryan8a@gmail.com <dryan8a@gmail.com>
2021-02-17 12:29:22 -0800

    Made the test for the Levenshtein algos and found the Partial Matrix to be faster significantly faster than Full Matrix, on top of the fact that it uses less memory. Next time, implement the partial matrix function in the main code

--- WordSuggestionsBenchmarkingTests/LevenshteinTests.cs
+++ WordSuggestionsBenchmarkingTests/LevenshteinTests.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using WordSuggestions;
+using System.Diagnostics;
+using System.IO;
 
 namespace WordSuggestionsBenchmarkingTests
 {
@@ -7,8 +9,32 @@ namespace WordSuggestionsBenchmarkingTests
     {
         static void Main(string[] args)
         {
+            var words = File.ReadAllLines("words_alpha.txt");
+            Stopwatch timer = new Stopwatch();
+            Random gen = new Random();
+            int testsToRun = 1000;
+
+            double fullMatrixSum = 0;
+            double partialMatrixSum = 0;
             
-            Console.WriteLine("Hello World!");
+            for(int i = 0;i<testsToRun;i++)
+            {
+                string firstWord = words[gen.Next(0,words.Length-1)];
+                string secondWord = words[gen.Next(0, words.Length - 1)];
+
+                timer.Restart();
+                _ = Levenshtein.GetFullMatrixDistance(firstWord, secondWord);
+                timer.Stop();
+                fullMatrixSum += timer.Elapsed.TotalMilliseconds;
+
+                timer.Restart();
+                _ = Levenshtein.GetPartialMatrixDistance(firstWord, secondWord);
+                timer.Stop();
+                partialMatrixSum += timer.Elapsed.TotalMilliseconds;
+            }
+
+            Console.WriteLine($"Full Matrix Algo Average Time for {testsToRun} tests: {fullMatrixSum / testsToRun} ms");
+            Console.WriteLine($"Partial Matrix Algo Average Time for {testsToRun} tests: {partialMatrixSum / testsToRun} ms");
         }
     }
 }
